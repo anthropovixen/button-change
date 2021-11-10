c5dcb3eb635f9022918eff2fc79597723b2f2cef
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-11-10 15:55:49 +1100

    Create project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Counter extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			count: 0,
+		};
+		this.increment = this.increment.bind(this);
+		this.decrement = this.decrement.bind(this);
+		this.reset = this.reset.bind(this);
+	}
+	increment() {
+		this.setState((state) => ({
+			count: state.count + 1,
+		}));
+	}
+	decrement() {
+		this.setState((state) => ({
+			count: state.count - 1,
+		}));
+	}
+	reset() {
+		this.setState({
+			count: 0,
+		});
+	}
+	render() {
+		return (
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+				}}
+			>
+				<button style={style} onClick={this.increment}>
+					+
+				</button>
+				<button style={style} id="decrement" onClick={this.decrement}>
+					-
+				</button>
+				<button style={style} id="reset" onClick={this.reset}>
+					Reset
+				</button>
+				<h1> Current Clicks: {this.state.count}</h1>
+			</div>
+		);
+	}
 }
 
-export default App;
+const style = {
+	border: '3px solid grey',
+	width: '100px',
+	height: '30px',
+	borderRadius: '5px',
+};
+export default Counter;
